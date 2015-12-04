e2befee029a4dd0947647b004360ec4127412a8c
XLekunberri <xlekunberri@gmail.com>
2015-12-04 13:41:37 +0100

    fAD egin gabe

--- Proiektua.hs
+++ Proiektua.hs
@@ -1,12 +1,11 @@
 praktika :: IO()
  praktika = do n<- alK
-  putStrLn"Adierazi CNF formula:"
-  f<-lZbZ
+  f<-fK n
   menu n f
 
   
   
-menu :: Integer -> [Integer] -> IO()
+menu :: Integer -> [[Integer]] -> IO()
  menu n f = do putStr("Sartutako CNF formula:"
   fAd n f
   aukeratu n f
@@ -22,7 +21,7 @@ alK :: IO Integer
   
   
   
-aukeratu :: IO ()
+aukeratu :: Integer -> [[Integer]] -> IO ()
  aukeratu n f = do putStr("Hautatu aukera bat:"++"\n"++"\n"++"1. Egiazkotasun-proba"++"\n"++"2. Betegarritasun-proba"++"\n"++"3. Baliozkotasun-proba"++"\n"++"4. Bukatu")
   a<-lZb
   if a==1
@@ -37,18 +36,57 @@ aukeratu :: IO ()
 
 	   
 	  
-egP :: Integer -> [Integer] -> IO()
+egP :: Integer -> [[Integer]] -> IO()
 
 
 
-beP :: Integer -> [Integer] -> IO()
+beP :: Integer -> [[Integer]] -> IO()
 
 
 
-baP :: Integer -> [Integer] -> IO()
+baP :: Integer -> [[Integer]] -> IO()
 
 
 
 lZb :: IO Integer
  lZb = do z <- getLine
   return (read z :: Integer)
+
+
+fK :: Integer -> IO [[Integer]]
+ fK n = do putStrLn"Adierazi CNF formula:"
+ return lZbZZ n
+
+
+lZbZZ :: Integer -> [[Integer]]
+ lZbZZ n = do putStrLn"Formula bat gehitu nahi duzu?"
+  if bEE
+  	then do  f <- getLine
+     read f :: [Integer]
+     if konprobatu f n
+ 	  then (f: lZbZZ n)
+ 	  else do putStrLn"Azken formula ez da zuzena"
+ 		      lZbZZ n 
+ 	else []
+
+
+bEE :: IO boolean
+ bEE = do putStr" B(Bai)-E(Ez)"
+ b <- getLine
+ read b :: string
+ if b == "b"
+ 	then return true
+ 	if b =="e"
+ 		then return false
+ 		else do putStrLn""
+ 		 bEE
+
+
+
+konprobatu :: [Integer] -> Integer -> boolean
+
+ konprobatu [] i = true
+ konprobatu x:s i
+  | i < 0 = false
+  | x==0 || x==1 || x==(-1) = konprobatu s (i-1)
+  | otherwise = false
